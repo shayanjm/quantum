a43dc6cae75ec9c7bce1a196b75fe9f9d8a62a73
Shayan Mohanty <shayan@evolucix.com>
2014-02-19 23:45:00 -0600

    one last time hopefully

--- Gruntfile.js
+++ Gruntfile.js
@@ -21,7 +21,7 @@ module.exports = function(grunt) {
         // The actual grunt server settings
         connect: {
             options: {
-                port: 9000,
+                port: process.env.PORT || 9000,
                 livereload: 35729,
                 // Change this to '0.0.0.0' to access the server from outside
                 hostname: 'localhost'
@@ -93,8 +93,7 @@ module.exports = function(grunt) {
     });
     grunt.registerTask('heroku:', function() {
         grunt.task.run([
-            'connect:livereload',
-            'watch'
+            'connect'
         ]);
     });
 
